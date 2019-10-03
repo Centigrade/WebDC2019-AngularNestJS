86a728ada7ab73738ce7648ce337952668e65f44
David Würfel <david.wuerfel@centigrade.de>
2019-10-03 15:45:46 +0200

    feat: implement getAll in CompanionsService and return sample characters

---
+++ ng-companion/src/app/data/sample-companions.ts
@@ -0,0 +1,9 @@
+import { Companion } from '../types';
+
+export const SAMPLE_COMPANIONS: Companion[] = [
+  { id: 'angular', name: 'Andre Gular', avatar: 'dragon2' },
+  { id: 'express', name: 'Eliza Xopress', avatar: 'robotrabbit' },
+  { id: 'vanilla-node', name: 'Vanilla Nodesto', avatar: 'deer' },
+  { id: 'adonisjs', name: 'Adonis Johnson', avatar: 'otter' },
+  { id: 'nestjs', name: 'Nesta Jason', avatar: 'dragon' },
+];
--- ng-companion/src/app/services/companions.service.ts
+++ ng-companion/src/app/services/companions.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { SAMPLE_COMPANIONS } from '../data/sample-companions';
+import { Companion } from '../types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CompanionsService {
-  constructor() {}
+  public getAll(): Observable<Companion[]> {
+    return of(SAMPLE_COMPANIONS);
+  }
 }
