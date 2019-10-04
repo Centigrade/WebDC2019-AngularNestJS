e94646d601ae9af449398346aae5ea1c1eac0565
David Würfel <david.wuerfel@centigrade.de>
2019-10-04 12:58:33 +0200

    feat: implement service connection to real backend

--- ng-companion/src/app/app.module.ts
+++ ng-companion/src/app/app.module.ts
@@ -1,15 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanionDetailComponent } from './components/companion-detail/companion-detail.component';
 import { CompanionsComponent } from './components/companions/companions.component';
-import { SimpleMarkdownPipe } from './pipes/simple-markdown.pipe';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { SimpleMarkdownPipe } from './pipes/simple-markdown.pipe';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent, CompanionsComponent, CompanionDetailComponent, SimpleMarkdownPipe, UserProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    CompanionsComponent,
+    CompanionDetailComponent,
+    SimpleMarkdownPipe,
+    UserProfileComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- ng-companion/src/app/services/companions.service.ts
+++ ng-companion/src/app/services/companions.service.ts
@@ -1,22 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { SAMPLE_COMPANIONS, SAMPLE_COMPANIONS_DETAILS } from '../data/sample-companions';
+import { Observable } from 'rxjs';
 import { Companion, CompanionDetails } from '../types';
+
+const BASE_URL = 'http://localhost:3000';
+const COMPANIONS_ROUTE = 'companions';
+const COMPANION_ROUTE = 'companion';
+const CONTACT_ROUTE = 'contact';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CompanionsService {
+  constructor(private http: HttpClient) {}
   public getAll(): Observable<Companion[]> {
-    return of(SAMPLE_COMPANIONS);
+    const url = `${BASE_URL}/${COMPANIONS_ROUTE}`;
+    return this.http.get<Companion[]>(url);
   }
 
   public getDetails(id: string): Observable<CompanionDetails> {
-    return of(SAMPLE_COMPANIONS_DETAILS.find(details => details.id === id));
+    const url = `${BASE_URL}/${COMPANION_ROUTE}/${id}`;
+    return this.http.get<CompanionDetails>(url);
   }
 
   public sayHello(myId: string, contactId: string): Observable<void> {
+    const url = `${BASE_URL}/${CONTACT_ROUTE}`;
+    const body = { myId, contactId };
     console.log(`Companion '${myId}' says hello to '${contactId}'.`);
-    return of();
+    return this.http.post<void>(url, body);
   }
 }
