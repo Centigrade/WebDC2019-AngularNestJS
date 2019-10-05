3bc7c422882c214d451883eaae24cc4695ea3658
David Würfel <david.wuerfel@centigrade.de>
2019-10-05 10:08:09 +0200

    feat: init swagger doc

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('ngCompanion Backend')
+    .setDescription('The ngCompanion API description')
+    .setVersion('1.0')
+    .addTag('companions')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
